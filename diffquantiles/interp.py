d98eb2d016b2ba7e981bf50f39b36bcc0877cab6
MilesCranmer <miles.cranmer@gmail.com>
2019-03-29 14:16:56 -0400

    Correct for shaped input and 1 feature

--- diffquantiles/interp.py
+++ diffquantiles/interp.py
@@ -4,5 +4,10 @@ import torch
 import numpy as np
 
 def interp(x, xp, fp):
-    return (((x - xp[:-1, None] > 0)*(xp[1:, None] - x > 0)).type(torch.FloatTensor) * \
-        (fp[:-1, None] + (x - xp[:-1, None]) * (fp[1:, None] - fp[:-1, None])/(xp[1:, None] - xp[:-1, None]))).sum(dim=0)
+    x = x.reshape(-1)
+    xp = xp.reshape(-1)
+    fp = fp.reshape(-1)
+    mask = ((x - xp[:-1, None] >= 0)*(xp[1:, None] - x > 0)).detach().type(x.type())
+    output = (mask * fp[:-1, None] + mask * (x - xp[:-1, None]) * (fp[1:, None] - fp[:-1, None])/(xp[1:, None] - xp[:-1, None])).sum(dim=0)
+    return output
+
--- diffquantiles/transform.py
+++ diffquantiles/transform.py
@@ -2,7 +2,7 @@
 
 import torch
 import numpy as np
-from .interp import interp
+from .interp import interp as interp
 
 
 BOUNDS_THRESHOLD = 1e-7
@@ -19,9 +19,9 @@ def inverse_transform_col(X_col, quantiles, references):
     X_col = interp(X_col, references, quantiles)
 
     lower_bounds_mask = (X_col - BOUNDS_THRESHOLD <
-                        lower_bound_y).type(torch.FloatTensor)
+                        lower_bound_y).type(X_col.type())
     upper_bounds_mask = (X_col + BOUNDS_THRESHOLD >
-                        upper_bound_y).type(torch.FloatTensor)
+                        upper_bound_y).type(X_col.type())
     
     X_col = X_col + upper_bounds_mask * (-X_col + upper_bound_y)
     X_col = X_col + lower_bounds_mask * (-X_col + lower_bound_y)
@@ -30,9 +30,14 @@ def inverse_transform_col(X_col, quantiles, references):
 
 def inverse_transform(X, quantiles, references):
     """Pass through fitted quantiles from scipy"""
+    all_features = []
     for feature_idx in range(X.shape[1]):
-        X[:, feature_idx] = inverse_transform_col(
+        all_features.append(inverse_transform_col(
             X[:, feature_idx], quantiles[:, feature_idx],
-            references)
+            references))
+    if len(all_features) > 1:
+        X = torch.cat(all_features, dim=1)
+    else:
+        X = all_features[0]
 
     return X
